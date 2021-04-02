6f7a3f9b3bc391637b67dc8926451043abfed4f2
pasketi <pasketi@gmail.com>
2021-04-02 18:39:24 +0300

    Driving is fun again :)

--- Assets/Scripts/Common/Constants/CPhysics.cs
+++ Assets/Scripts/Common/Constants/CPhysics.cs
@@ -2,7 +2,7 @@
 {
     public class CPhysics
     {
-        public const float GRAVITY_ACCELERATION = 9.81f;
+        public const float GRAVITY_ACCELERATION = 5f;
         public const float INVERTED_GRAVITY_ACCELERATION = 1 / GRAVITY_ACCELERATION;
     }
 }
